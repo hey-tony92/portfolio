3358b77a8a1783f01a7be2c4d69414f1264322a8
Anthony <anthony.crisa92@gmail.com>
2025-05-14 19:03:00 -0400

    made contact form responsive and function with form submission

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ document.addEventListener("DOMContentLoaded", function() {
    });
 });
 
+//form submission confirmation
+
+document.querySelector('form').addEventListener('submit', async function(e) {
+    e.preventDefault();
+
+    try {
+        const response = await fetch('https://formspree.io/f/xqaqwqqr', {
+            method: 'POST',
+            body: new FormData(this),
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if(response.ok) {
+            this.reset(); //clears the form
+
+            //show success message
+            const formContainer = document.getElementById('form-container');
+            formContainer.innerHTML = `
+                <div class="success-message">
+                    <h2>Message Sent!</h2>
+                    <p>Thank you! I'll get back to you shortly.</p>
+                </div>`;
+        }
+    } catch (error) {
+        console.error('Error', error);
+        alert('Whoops! There was a problem submitting your message. Try again!');
+    }
+});
